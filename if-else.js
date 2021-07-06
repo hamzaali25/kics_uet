c48f4e4e7a77ee211f470087b9701a9461b6feff
hamzaali25 <75741363+hamzaali25@users.noreply.github.com>
2021-07-06 12:36:14 +0500

    Add files via upload

---
+++ if-else.js
@@ -0,0 +1,20 @@
+//if (condition) {
+//    statement
+//}
+//else if (condition) {
+//    statement
+//}
+
+let hours = 6 ;
+
+if (hours >= 6 && hours < 12) {
+    console.log('Good Morning!'); 
+}
+
+else if (hours >= 12 && hours < 18) {
+    console.log('Good Afternoon!');
+}
+
+else {
+    console.log('Good Evening!');
+}
